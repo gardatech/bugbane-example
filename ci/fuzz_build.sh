0635fc0d194a9d76167089d2f3c69291e3f15c3d
fuzzah <fuzzah@tuta.io>
2024-02-21 06:20:06 +0300

    update example: move to ubuntu, use 4.08c

--- ci/fuzz_build.sh
+++ ci/fuzz_build.sh
@@ -1,5 +1,10 @@
-export CXX="${CXX:=afl-clang-fast++}" &&
-mkdir -p build &&
-make clean &&
-make -j obj/libre2.a &&
+#!/bin/bash
+set -euxo pipefail
+
+export CXX="${CXX:=afl-clang-fast++}"
+
+rm -rf build
+mkdir -p build
+make clean
+make -j obj/libre2.a
 $CXX $CXXFLAGS --std=c++11 -I. re2/fuzzing/re2_fuzzer.cc /AFLplusplus/libAFLDriver.a obj/libre2.a -lpthread -o build/re2_fuzzer
